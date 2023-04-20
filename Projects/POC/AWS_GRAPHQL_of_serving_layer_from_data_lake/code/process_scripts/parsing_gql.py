127f38ca7cf8bd015f3aadf70094c2ac44c7efc5
Carlos Chicata <carlos.fernando.chicata@gmail.com>
2023-04-19 22:11:09 -0500

    feat: add base code to generate a node

--- Projects/POC/AWS_GRAPHQL_of_serving_layer_from_data_lake/code/process_scripts/parsing_gql.py
+++ Projects/POC/AWS_GRAPHQL_of_serving_layer_from_data_lake/code/process_scripts/parsing_gql.py
@@ -1,6 +1,60 @@
+def generate_format_request(level, gql_request, format_obj, name_level ):
+    try:
+        rich_fields = [ (field, field.count('/')) for field in gql_request]
+        
+        # setting format obj
+        format_obj["field"] = []
+        format_obj["name"] = name_level
+        format_obj["node"] = []
+        
+        # set up variables for process
+        temp_gql_request = []
+        node_fields = []
+
+        # add only field of level
+        for field in rich_fields:
+            if field[1] == level:
+                #print(field)
+                format_obj["field"].append(field[0])
+            else:
+                temp_gql_request.append(field)
+        
+        #print(format_obj)
+        #print(temp_gql_request)
+        #print("--------------")
+        
+        # check what is the node field in this level
+        for field in temp_gql_request:
+            inner_field = field[0].split("/")[0]
+                
+            if inner_field in format_obj["field"] and inner_field not in node_fields:
+                node_fields.append(inner_field)
+                format_obj["field"].remove(inner_field)
+                format_obj["node"].append([])
+
+        #print(format_obj)
+        #print(node_fields)
+        #print(temp_gql_request)
+        #print("--------------")
+
+        # fill node of level with field need to formatting
+        for field in temp_gql_request:
+            inner_field = field[0].split("/")[0]
+            index_node = node_fields.index(inner_field)
+            format_obj["node"][index_node].append(field[0])
+        
+        # remove field not needed
+        
+        
+        print(format_obj)
+        
+        return format_obj
+    except Exception as e:
+        print(e)
+        raise e  
 
 
-
+# working
 def parsing_gql_request_to_obj(fields):
     try:
         # Step 1: get the level of deep by fields
@@ -9,6 +63,10 @@ def parsing_gql_request_to_obj(fields):
 
         format_request = {}
         #  Step 2: setup to recorring element
+        generate_format_request(0, fields, {}, 'Client')
+        #while len(rich_fields) > 0:
+        #    True
+        
         return format_request
     except Exception as e:
         print(e)
@@ -31,4 +89,14 @@ GQL = [
       'enterprise_key' # ->  1
     ]
 
+GQL2 = [
+    'cityID/id',
+    'cityID/countryID', 
+    'cityID/countryID/currencyISO', 
+    'cityID/countryID/id', 
+    'cityID/countryID/name', 
+    'cityID/name'
+]
+
+#generate_format_request(0, GQL, {}, "Client")
 parsing_gql_request_to_obj(GQL)
