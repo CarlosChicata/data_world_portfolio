db7dfacdc68b4d244ebeced8a602981286eb50bf
Carlos Chicata <carlos.fernando.chicata@gmail.com>
2023-04-29 19:19:21 -0500

    fix: get data from aws athena process

--- Projects/POC/AWS_GRAPHQL_of_serving_layer_from_data_lake/code/process_scripts/process_graph_request.py
+++ Projects/POC/AWS_GRAPHQL_of_serving_layer_from_data_lake/code/process_scripts/process_graph_request.py
@@ -24,27 +24,32 @@ ATHENA_WORKGROUP = os.environ["ATHENA_WORKGROUP"]
 ATHENA_S3_BUCKET_OUTPUT = os.environ["ATHENA_S3_BUCKET_OUTPUT"]
 '''
 
-# working ....
+# OK
 def get_data_from_sql_engine(query):
     '''
-        Execute a SQL sentences and return data.
+        Execute a SQL sentences in AWS AThena and return data.
+        
+        Params
+        query (string): sql query to extract data
+        
+        return a location of generated file: key of file and bucket
     '''
     try:
         # setting params to control de Athena
         STATE = "RUNNING"
-        #MAX_EXECUTION = 10
-        
+
         ## STEP 1 : go the SQL sentence to athena
         
         query_id = athena_cli.start_query_execution(
             QueryString = query,
-            ResultConfiguration= {"OutputLocation": ATHENA_S3_OUTPUT},
-            WorkGroup = ATHENA_WORKGROUP
+            QueryExecutionContext = {
+                "Database": "db-poc-case-1"
+            },
+            ResultConfiguration= {"OutputLocation": ATHENA_S3_OUTPUT}
         )
 
         ## STEP 2 : waiting the finish operation: asynchronic ops
 
-        #while MAX_EXECUTION > 0 and STATE in ["RUNNING", "QUEUED"]:
         while STATE in ["RUNNING", "QUEUED"]:
             #MAX_EXECUTION -= 1
             response = athena_cli.get_query_execution(
@@ -58,29 +63,20 @@ def get_data_from_sql_engine(query):
                 STATE = response["QueryExecution"]["Status"]["State"]
 
                 if STATE == 'FAILED' or STATE == 'CANCELLED':
+                    print(STATE)
                     raise Exception("error: not allow to get data; maybe it was failed or cancelled.")
                 if STATE == "SUCCEEDED":
                     print("Get data!")
                     break
 
-            time.sleep(8)
-
         ## STEP 3 : get data of query
         file_query_solved = query_id["QueryExecutionId"] + ".csv"
         
-        response = s3_cli.get_object(
-            Bucket=  ATHENA_S3_BUCKET_OUTPUT,
-            Key=file_query_solved
-        )
-        
-        df_solved = pd.read_csv(io.BytesIO(response['Body'].read()), encoding='utf8')
-        
-        return None
+        return file_query_solved, ATHENA_S3_OUTPUT
     except Exception as e:
         print("error")
         print(e)
         raise e
-
 
 def processing_graphql_request(gql_request_field):
     '''
