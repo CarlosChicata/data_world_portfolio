17069fb1f9540d5e8fa36985b7ae095265b6bdb3
Carlos Chicata <carlos.fernando.chicata@gmail.com>
2023-05-02 22:36:27 -0500

    feat: add emulator of real time tweets extractor

--- Projects/POC/AWS_Ingesting_disaster_tweets_in_real_time/process_scripts/real_time_sender.py
+++ Projects/POC/AWS_Ingesting_disaster_tweets_in_real_time/process_scripts/real_time_sender.py
@@ -2,10 +2,15 @@
     Process to emulate the send of tweets in real time enviroment
 '''
 import time
+import json
+import random
 
+
+import requests
 import pandas as pd
 
 
+# ok
 def get_data_from_file(filename):
     '''
         Get a data in data frame format from file.
@@ -18,5 +23,51 @@ def get_data_from_file(filename):
     return pd.read_csv(filename, encoding="latin9")
 
 
+# need to test
+def http_request_send(url, data):
+    '''
+        Send a tweet to infra from http request
+        
+        Args
+        url (string): url will receive the tweets.
+        data (object) List of tweets will process.
+        
+        return none
+    '''
+    headers_req = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    requests.post(url, data=json.dumps(data), headers=headers_req)
 
-print(get_data_from_file("./sample_data/database.csv"))
+
+# ok
+def real_time_generator_emulator(filename, url):
+    '''
+        generate a emulator to send collected tweets in real time
+        
+        Args
+        filename (string): location of tweets database.
+        url (string): location of service will receive the tweets.
+        
+        return none.
+    '''
+    database = get_data_from_file(filename)
+    database_size = database.shape[0]
+    
+    count_tweets = 0
+    
+    while count_tweets < database_size:
+        chunk_size = random.randint(1, 10)
+        second_sleepy = random.randint(1, 30)
+        
+        selected_tweets = database.iloc[count_tweets : count_tweets + chunk_size]
+        selected_tweets = selected_tweets.to_dict(orient="records")
+        print(selected_tweets)
+        print(type(selected_tweets))
+        print(second_sleepy)
+        print(chunk_size)
+        
+        count_tweets += chunk_size
+        http_request_send(url, selected_tweets)
+        time.sleep(second_sleepy)
+    
+
+real_time_generator_emulator("./sample_data/database.csv", "FAKE URL SERVICE")
