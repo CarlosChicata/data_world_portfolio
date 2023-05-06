10422feceef8396da5a2eb0ac6672c969294df4b
Carlos Chicata <carlos.fernando.chicata@gmail.com>
2023-05-06 09:50:16 -0500

    fix: change encode format of text

--- Projects/POC/AWS_Ingesting_disaster_tweets_in_real_time/process_scripts/transform_firehose_lambda.py
+++ Projects/POC/AWS_Ingesting_disaster_tweets_in_real_time/process_scripts/transform_firehose_lambda.py
@@ -7,15 +7,14 @@ def lambda_handler(event, context):
     output = []
 
     for record in event['records']:
-        print(record['recordId'])
-        payload = base64.b64decode(record['data']).decode('utf-8')
+        payload = base64.b64decode(record['data']).decode('latin9') + "\n"
 
         # Do custom processing on the payload here
 
         output_record = {
             'recordId': record['recordId'],
             'result': 'Ok',
-            'data': base64.b64encode(payload.encode('utf-8')).decode('utf-8')
+            'data': base64.b64encode(payload.encode('latin9')).decode('latin9')
         }
         output.append(output_record)
 
