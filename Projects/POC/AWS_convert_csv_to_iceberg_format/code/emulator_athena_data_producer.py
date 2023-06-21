271bbd68328480f9ad0c2a2d540ba3d274e704c3
Carlos Chicata <carlos.fernando.chicata@gmail.com>
2023-06-20 21:59:27 -0500

    feat: add executor query in Athena POC 4

--- Projects/POC/AWS_convert_csv_to_iceberg_format/code/emulator_athena_data_producer.py
+++ Projects/POC/AWS_convert_csv_to_iceberg_format/code/emulator_athena_data_producer.py
@@ -1,8 +1,9 @@
-import chunk
+import time
+
 from boto3 import Session
 import pandas as pd
 
-'''
+
 ACCESS_KEY = ""
 SECRET_KEY = ""
 
@@ -12,11 +13,58 @@ session = Session(
 )
 
 athena_cli = session.client("athena", region_name="us-east-1")
-'''
+S3_OUTPUT = ""
+
+
+def send_data_to_athena_table(sql_query):
+    '''
+        execute the SQL query in athena
+
+        Params
+        sql_query (string): SQL query to run.
+        return None
+    '''
+    STATE = "RUNNING"
+
+    ## STEP 1 : go the SQL sentence to athena
+    query_id = athena_cli.start_query_execution(
+        QueryString = sql_query,
+        ResultConfiguration= {"OutputLocation": S3_OUTPUT}
+    )
+    print("request of query: ", query_id)
+
+    ## STEP 2 : waiting the finish operation: asynchronic ops
+
+    while  STATE in ["RUNNING", "QUEUED"]:
+        response = athena_cli.get_query_execution(
+                QueryExecutionId=query_id["QueryExecutionId"]
+            )
+
+        if "QueryExecution" in response and \
+            "Status" in response["QueryExecution"] and \
+            "State" in response["QueryExecution"]["Status"]:
+
+            STATE = response["QueryExecution"]["Status"]["State"]
+            if STATE == 'FAILED' or STATE == 'CANCELLED':
+                print("error in execution of query")
+                raise Exception("error: not allow to get data; maybe it was failed or cancelled.")
+            if STATE == "SUCCEEDED":
+                print("Get data!")
+                break
+        
+        time.sleep(10)
+
+    return None
 
 
 def sending_batch_data(size_chunk):
     '''
+        Send data in batch mode from csv format file to iceberg file by AWS Athena.
+
+        Params:
+        size_chunk (int): number of element in chunk.
+
+        return none
     '''
     if size_chunk >= 100:
         raise Exception("The size_chunk can't be more 100 because avoid limit in partitions athena.")
