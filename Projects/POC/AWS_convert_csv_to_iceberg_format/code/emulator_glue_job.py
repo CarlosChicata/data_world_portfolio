8a42a948b06852700aac354a392c8f511334fd03
Carlos Chicata <carlos.fernando.chicata@gmail.com>
2023-06-23 11:40:03 -0500

    feat: finish emulator csv to iceberg transform POC 4

--- Projects/POC/AWS_convert_csv_to_iceberg_format/code/emulator_glue_job.py
+++ Projects/POC/AWS_convert_csv_to_iceberg_format/code/emulator_glue_job.py
@@ -1,16 +1,17 @@
 import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
-from pyspark.context import SparkContext
-from pyspark.conf import SparkConf
-from awsglue.context import GlueContext
+from pyspark.context import SparkConf, SparkContext
 from pyspark.sql import SparkSession
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql.functions import col
 
 
 # get passed params from aws glue job
 args = getResolvedOptions(
     sys.argv, 
-    ['bucket_origin', "object_bucket_origin"]
+    ['JOB_NAME','bucket_origin', "object_bucket_origin"]
 )
 
 # check
@@ -20,40 +21,46 @@ object_bucket_origin = args["object_bucket_origin"]
 print(bucket_origin, object_bucket_origin, sep="\n")
 
 # setup the spark session in my aws glue job
-conf = SparkConf()\
-    .set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")\
-    .set("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog")\
-    .set("spark.sql.catalog.glue_catalog.warehouse", "s3://s3-storage-layer/athena/")\
-    .set("spark.sql.catalog.glue_catalog.catalog-impl","org.apache.iceberg.aws.glue.GlueCatalog")\
-    .set("spark.sql.catalog.glue_catalog.io-impl","org.apache.iceberg.aws.s3.S3FileIO")
-sc = SparkContext.getOrCreate()
-glueContext = GlueContext(sc)
-spark = glueContext.spark_session
+conf_list = [
+    #General Spark configs
+    ("hive.metastore.client.factory.class", "com.amazonaws.glue.catalog.metastore.AWSGlueDataCatalogHiveClientFactory"),
+    ("spark.sql.parquet.writeLegacyFormat", "true"),
+    ("spark.sql.parquet.writeLegacyFormat", "true"),
+    ("hive.exec.dynamic.partition.mode", "nonstrict"),
+    ("spark.sql.hive.caseSensitiveInferenceMode", "INFER_ONLY"),
+    ("spark.sql.source.partitinoOverviewMode", "dynamic"),
+    #Configs needed for Iceberg
+    ("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions"),
+    ("spark.sql.catalog.iceberg_catalog", "org.apache.iceberg.spark.SparkCatalog"),
+    ("spark.sql.catalog.iceberg_catalog.warehouse", "s3://s3-storage-layer/iceberg_catalog/"),
+    ("spark.sql.catalog.iceberg_catalog.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog")
+]
+ 
+spark_conf = SparkConf().setAll(conf_list)
+spark = SparkSession.builder.config(conf=spark_conf).enableHiveSupport().getOrCreate()
+glue_context = GlueContext(spark.sparkContext)
+job = Job(glue_context)
+job.init(args['JOB_NAME'], args)
 
 # read the csv file in dynamicframe obj.
 csv_file_s3_url = "s3://" +  bucket_origin + "/" + object_bucket_origin
 
 print(csv_file_s3_url)
 
-dynamicFrame = glueContext.create_dynamic_frame.from_options(
-    connection_type="s3",
-    connection_options={"paths": [csv_file_s3_url]},
-    format="csv",
-    format_options={
-        "withHeader": True,
-    }
-)
+df = spark.read.format("csv").option("delimiter", ";").option("header", "true").load(csv_file_s3_url)
 
-dataFrame = dynamicFrame.toDF()
+# create tem tamble with data from csv file inside spark
+df.registerTempTable("csv_data_orders")
 
 # write a iceberg table in glue catalog using csv file
-
-dataFrame.createOrReplaceTempView("tmp_csv_to_iceberg_order_glue")
-
-query = f"""
-    CREATE TABLE glue_catalog.db-poc-case-4.csv_to_iceberg_order_glue
-    USING iceberg
-    AS SELECT * FROM tmp_csv_to_iceberg_order_glue;
+sql_stmnt = f"""
+CREATE OR REPLACE TABLE iceberg_catalog.db_poc_for_glue.csv_to_iceberg_glue
+USING iceberg
+TBLPROPERTIES ('table_type'='ICEBERG', 'format-version'='2', 'format'='parquet')
+LOCATION 's3://s3-storage-layer/glue/data/db_poc_for_glue/csv_to_iceberg_glue'
+AS SELECT * FROM csv_data_orders
 """
+spark.sql(sql_stmnt).show()
 
-spark.sql(query)
+job.commit()
+
