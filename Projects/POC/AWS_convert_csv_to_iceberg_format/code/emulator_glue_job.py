2e0207b82cbc12f6e16f23581ba981e7288e2a8e
Carlos Chicata <carlos.fernando.chicata@gmail.com>
2023-06-22 20:38:54 -0500

    feat: csv to iceberg transformer POC 4

--- Projects/POC/AWS_convert_csv_to_iceberg_format/code/emulator_glue_job.py
+++ Projects/POC/AWS_convert_csv_to_iceberg_format/code/emulator_glue_job.py
@@ -2,6 +2,7 @@ import sys
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
+from pyspark.conf import SparkConf
 from awsglue.context import GlueContext
 from pyspark.sql import SparkSession
 
@@ -9,18 +10,22 @@ from pyspark.sql import SparkSession
 # get passed params from aws glue job
 args = getResolvedOptions(
     sys.argv, 
-    ['bucket_origin', "bucket_destiny", "object_bucket_origin", "object_bucket_destiny"]
+    ['bucket_origin', "object_bucket_origin"]
 )
 
 # check
 bucket_origin = args["bucket_origin"]
-bucket_destiny = args["bucket_destiny"]
 object_bucket_origin = args["object_bucket_origin"]
-object_bucket_destiny = args["object_bucket_destiny"]
 
-print(bucket_origin, bucket_destiny, object_bucket_origin, object_bucket_destiny, sep="\n")
+print(bucket_origin, object_bucket_origin, sep="\n")
 
 # setup the spark session in my aws glue job
+conf = SparkConf()\
+    .set("spark.sql.extensions", "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions")\
+    .set("spark.sql.catalog.glue_catalog", "org.apache.iceberg.spark.SparkCatalog")\
+    .set("spark.sql.catalog.glue_catalog.warehouse", "s3://s3-storage-layer/athena/")\
+    .set("spark.sql.catalog.glue_catalog.catalog-impl","org.apache.iceberg.aws.glue.GlueCatalog")\
+    .set("spark.sql.catalog.glue_catalog.io-impl","org.apache.iceberg.aws.s3.S3FileIO")
 sc = SparkContext.getOrCreate()
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
@@ -39,16 +44,16 @@ dynamicFrame = glueContext.create_dynamic_frame.from_options(
     }
 )
 
-# write the parquet file from dynamicframe obj.
-parquet_file_s3_url = "s3://" +  bucket_destiny + "/" + object_bucket_destiny
+dataFrame = dynamicFrame.toDF()
 
-glueContext.write_dynamic_frame.from_options(
-    frame=dynamicFrame,
-    connection_type="s3",
-    format="parquet",
-    connection_options={
-        "path": parquet_file_s3_url,
-    }
-)
+# write a iceberg table in glue catalog using csv file
 
+dataFrame.createOrReplaceTempView("tmp_csv_to_iceberg_order_glue")
 
+query = f"""
+    CREATE TABLE glue_catalog.db-poc-case-4.csv_to_iceberg_order_glue
+    USING iceberg
+    AS SELECT * FROM tmp_csv_to_iceberg_order_glue;
+"""
+
+spark.sql(query)
