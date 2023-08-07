b61bb7f01ecbf7861c5b79f1e040573cb8b4da52
Carlos Chicata <carlos.fernando.chicata@gmail.com>
2023-08-06 23:31:18 -0500

    feat: add base to create SQL command in processing lambda POC 5

--- Projects/POC/AWS_API_of_serving_layer_from_data_lake_v2/code/process_script_v2/processing_lambda.py
+++ Projects/POC/AWS_API_of_serving_layer_from_data_lake_v2/code/process_script_v2/processing_lambda.py
@@ -12,6 +12,10 @@ def lambda_handler(event, context):
     # TODO implement
     print(event)
     context_fields = event["requestContext"]["authorizer"]["lambda"]
+    columns = ", ".join(context_fields["columns"])
+    command = context_fields["command"]
+    enterprise_key = context_fields["enterprise_key"]
+    command_sql = command % ('2023-02-10 00:00', '2023-05-10 00:00', enterprise_key, columns)
     return {
         'statusCode': 200,
         'body': json.dumps('Hola Carlitos o Jessica o Zayda o Melita o Ariani o Ethel')
